e3da8206234523233ea5bc09aa25011c66bcc7ef
Lawrence Goldstien <lawrence@simplecat.software>
2019-02-11 16:19:23 +0000

    use winston for logging

--- server.js
+++ server.js
@@ -1,9 +1,20 @@
 require('dotenv').config();
 const express = require('express');
+const winston = require('winston');
+const config = require('./package');
+
+const logger = winston.createLogger({
+    level: 'info',
+    format: winston.format.json(),
+    defaultMeta: { service: `${config.name}-server` },
+    transports: [
+        new winston.transports.Console(),
+    ],
+});
 
 const app = express();
 
 app.listen(
     process.env.PORT,
-    () => { console.log(`listening on port ${process.env.PORT}`) }
+    () => { logger.info(`listening on port ${process.env.PORT}`) }
 );
