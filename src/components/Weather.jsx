727916d4a3d690e5458cd822d76c0ece0afa48ca
Lawrence Goldstien <lawrence@simplecat.software>
2019-02-12 21:58:10 +0000

    create Weather component

---
+++ src/components/Weather.jsx
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import {Col, Row} from "reactstrap";
+
+export default class Weather extends Component {
+    render() {
+        return (
+            <Row>
+                <Col>
+
+                </Col>
+            </Row>
+        );
+    }
+}
