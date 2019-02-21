e85d4a5ad368d2cb217b8143e085a2bf486ea6b0
Lawrence Goldstien <lawrence@simplecat.software>
2019-02-21 20:57:45 +0000

    add api calls backed up in localStorage

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,11 +1,55 @@
 import React, {Component} from 'react';
+import {get} from 'lodash';
 import {Card, CardHeader} from "reactstrap";
 import propTypes from '../propTypes';
+import Api from '../libs/Api';
 
 export default class Weather extends Component {
     static propTypes = {
         location: propTypes.location,
     };
+
+    constructor(props) {
+        super(props);
+
+        this.state = {weather: null};
+
+        this.getWeather = this.getWeather.bind(this);
+        this.setWeather = this.setWeather.bind(this);
+    }
+
+    componentDidMount() {
+        this.getWeather();
+    }
+
+    getWeather() {
+        let weather = null;
+
+        try {
+            weather = JSON.parse(localStorage.getItem(`weather-${this.props.location.woeid}`));
+        } catch (e) {
+        }
+
+        if (
+            weather
+            && get(weather, 'consolidated_weather[0].applicable_date') === (new Date()).toJSON().slice(0, 10)
+        ) {
+            return this.setWeather(weather);
+        }
+
+        Api.weather(this.props.location.woeid)
+            .then(weather => {
+                this.setWeather(weather);
+            });
+    }
+
+    setWeather(weather) {
+        localStorage.setItem(`weather-${this.props.location.woeid}`, JSON.stringify(weather));
+        this.setState(prevState => ({
+            ...prevState,
+            weather,
+        }));
+    }
 
     render() {
         return (
