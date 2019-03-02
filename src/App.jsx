eeb82395b8e10aa36f88931fe240028e4f0f7983
Lawrence Goldstien <lawrence@simplecat.software>
2019-03-02 22:07:13 +0000

    add a background and proper spacing for footer

--- src/App.jsx
+++ src/App.jsx
@@ -42,7 +42,7 @@ export default class App extends Component {
 
     render() {
         return (
-            <div>
+            <div style={{marginBottom: 75}}>
                 <Menu>
                     <Search onLocationsUpdated={this.onLocationsUpdated}
                             selectedLocations={this.state.locations}
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ import {Navbar} from "reactstrap";
 export default class Footer extends Component {
     render() {
         return (
-            <Navbar color="faded" light fixed={'bottom'}>
+            <Navbar color={'light'} light fixed={'bottom'}>
                 <div className={'navbar-text text-center mx-auto'}>
                     Developed by Simple Cat Software<br/>
                     Powered by data from <a href={'https://www.metaweather.com/'} target={'_blank'}>MetaWeather</a>
