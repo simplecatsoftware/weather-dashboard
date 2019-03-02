e8fe42e2b941bde1f21aab122d0bfc2d8a81bf7c
Lawrence Goldstien <lawrence@simplecat.software>
2019-03-02 22:11:36 +0000

    syntax cleanup

--- src/App.jsx
+++ src/App.jsx
@@ -33,7 +33,7 @@ export default class App extends Component {
         this.setState(prevState => ({
             ...prevState,
             locations,
-        }))
+        }));
     }
 
     onLocationsUpdated(locations) {
