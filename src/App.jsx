b240e03d45c78939c9dee3d07f77cfd39a6cb0ef
Lawrence Goldstien <lawrence@simplecat.software>
2019-03-02 14:19:29 +0000

    move code previously in dashboard to app

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,65 @@
 import React, {Component} from 'react';
+import {Col, Container, Row} from "reactstrap";
+import Menu from "./components/Menu";
+import Search from "./components/Search";
+import Weather from "./components/Weather";
 
 import './App.scss';
-import Dashboard from "./components/Dashboard";
 
 export default class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            locations: [],
+        };
+
+        this.onLocationsUpdated = this.onLocationsUpdated.bind(this);
+    }
+
+    componentDidMount() {
+        let locations = [];
+
+        try {
+            locations = JSON.parse(localStorage.getItem('locations')) || [];
+        } catch (e) {
+        }
+
+        this.setState(prevState => ({
+            ...prevState,
+            locations,
+        }))
+    }
+
+    onLocationsUpdated(locations) {
+        this.setState(prevState => {
+            return {...prevState, locations};
+        }, () => {
+            localStorage.setItem('locations', JSON.stringify(locations));
+        });
+    }
+
     render() {
         return (
-            <Dashboard/>
+            <div>
+                <Menu>
+                    <Search onLocationsUpdated={this.onLocationsUpdated}
+                            selectedLocations={this.state.locations}
+                    />
+                </Menu>
+                <Container fluid>
+                    <Row>
+                        {this.state.locations.map(location =>
+                            <Col className={`mb-3`}
+                                 key={`weather-${location.woeid}`}
+                                 md={6}
+                            >
+                                <Weather location={location}/>
+                            </Col>
+                        )}
+                    </Row>
+                </Container>
+            </div>
         );
     }
 }
