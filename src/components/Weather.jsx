14e1e8d0d9bce13a6495fb502754bd11f2f96641
Lawrence Goldstien <lawrence@simplecat.software>
2019-03-02 15:15:16 +0000

    display weather information

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,8 +1,10 @@
 import React, {Component} from 'react';
 import {get} from 'lodash';
-import {Card, CardHeader} from "reactstrap";
+import {Card, CardBody} from "reactstrap";
+import moment from 'moment';
 import propTypes from '../propTypes';
 import Api from '../Api';
+import WeatherIcon from "./WeatherIcon";
 
 export default class Weather extends Component {
     static propTypes = {
@@ -54,10 +56,26 @@ export default class Weather extends Component {
     render() {
         return (
             <Card>
-                <CardHeader>
-                    {this.props.location.title}
-                </CardHeader>
-
+                <CardBody>
+                    {this.state.weather.length > 0 &&
+                        <div className="media">
+                            <WeatherIcon className={`align-self-center mr-3`}
+                                         condition_long={this.state.weather[0].weather_state_name}
+                                         condition_short={this.state.weather[0].weather_state_abbr}
+                                         featured
+                            />
+                            <div className="media-body text-right my-auto">
+                                <h2>{this.props.location.title}</h2>
+                                <h3>{this.state.weather[0].the_temp.toFixed(0)}&deg;C</h3>
+                                <h4>
+                                    {this.state.weather[0].weather_state_name}&nbsp;
+                                    {moment(this.state.weather[0].applicable_date).calendar().split(' ')[0]}
+                                </h4>
+                                <h5>Accuracy {this.state.weather[0].predictability.toFixed(0)}%</h5>
+                            </div>
+                        </div>
+                    }
+                </CardBody>
             </Card>
         );
     }
